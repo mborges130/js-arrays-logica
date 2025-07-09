a4ab4c29c2647e8985b2d0c5248ab9204b9685e4
Mateus Borges <mateusdantas73@icloud.com>
2025-07-09 15:40:59 -0300

    percorrendo em arrays

---
+++ for-classico.js
@@ -0,0 +1,8 @@
+const notas = [10,8.5,5,6.5,8,7.5];
+// primeira exp.: executada apenas uma vez
+//segunda exp.: condição de execução
+// terceira exp.: executada sempre ao final do bloco
+for (let indice = 0; indice < notas.length; indice++){
+    console.log(indice, notas[indice]);
+    
+}
