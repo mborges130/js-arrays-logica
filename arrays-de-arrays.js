e3fa94b56c56a60cfe60c1467ac94173cf05d717
Mateus Borges <mateusdantas73@icloud.com>
2025-07-09 12:07:27 -0300

    arrays de arrays

---
+++ arrays-de-arrays.js
@@ -0,0 +1,9 @@
+const nomeEstudantes = ['João', 'Juliana', 'Caio', 'Ana'];
+const notas = [10, 8, 7.5, 9];
+
+const lista = [nomeEstudantes,notas];
+
+console.log(
+    `a aluna da posição 1 da lista é: ${lista[0][1]}.
+    a nota desta aluna é ${lista[1][1]}`
+);
